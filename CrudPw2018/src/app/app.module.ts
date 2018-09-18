239fa46b5d1037a369724a817ce0972816d9a6a7
Pablo David Aguilar Barco <pablo.aguilar@mobilityscm.com>
2018-09-18 16:45:23 -0600

    Proyecto: Lab02 Descripción: Se carga Home Page de proyecto.

--- CrudPw2018/src/app/app.module.ts
+++ CrudPw2018/src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from './navbar/navbar.component';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+// import { RegistrationComponent } from './registration/registration.component';
+
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NavbarComponent, HomeComponent],
+  imports: [BrowserModule, AppRoutingModule, NgbModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
